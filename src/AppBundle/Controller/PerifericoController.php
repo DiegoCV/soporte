a51385a81e89d502000749308bb4c7ac8dea3ef0
El grandioso ingeniero Arciniegas <fredy_yamid@hotmail.com>
2017-12-16 15:56:51 -0500

    mi primer commit :)

---
+++ src/AppBundle/Controller/PerifericoController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method; // Permite definir que tipo de metodo vamos a usar EJM(POST GET DELETE PUT)
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+use AppBundle\Entity\Equipo;
+
+class PerifericoController extends Controller
+{
+
+	/**
+     * @Route("/listar", name="listar")
+     * Method({'GET'})
+     */
+	public function listar(Request $request)
+	{
+
+		$equipos = $this->getDoctrine()->getRepository('AppBundle:Equipo')->findAll();
+
+		//var_dump($equipos);
+
+		return $this->render('listado.html.twig', array('equipos' => $equipos));
+
+	}
+        public function insertar(Request $request){
+            $perifericos=$this->getDoctrine()->getRepository('AppBundle:Perifericos')->;
+        }
+}
